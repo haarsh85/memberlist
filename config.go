2fa55f141a6fcf6ca264a99b14147f3de0784fca
Harsha Nanayakkara <harsh.life@hotmail.com>
2025-03-25 18:37:15 +0100

    update config.go

--- config.go
+++ config.go
@@ -308,14 +308,14 @@ func DefaultLANConfig() *Config {
 		AdvertiseAddr:           "",
 		AdvertisePort:           7946,
 		ProtocolVersion:         ProtocolVersion2Compatible,
-		TCPTimeout:              30 * time.Second,       // Timeout after 10 seconds
+		TCPTimeout:              15 * time.Second,       // Timeout after 10 seconds
 		IndirectChecks:          3,                      // Use 3 nodes for the indirect ping
-		RetransmitMult:          2,                      // Retransmit a message 4 * log(N+1) nodes
-		SuspicionMult:           2,                      // Suspect a node for 4 * log(N+1) * Interval
+		RetransmitMult:          1.5,                      // Retransmit a message 4 * log(N+1) nodes
+		SuspicionMult:           1.5,                      // Suspect a node for 4 * log(N+1) * Interval
 		SuspicionMaxTimeoutMult: 8,                      // For 10k nodes this will give a max timeout of 120 seconds
-		PushPullInterval:        150 * time.Second,       // Low frequency
-		ProbeTimeout:            2 * time.Second, // Reasonable RTT time for LAN
-		ProbeInterval:           120 * time.Second,        // Failure check every second
+		PushPullInterval:        90 * time.Second,       // Low frequency
+		ProbeTimeout:            500 * time.Millisecond, // Reasonable RTT time for LAN (How long to wait for a response when checking if a node is alive)
+		ProbeInterval:           45 * time.Second,        // Failure check every second
 		DisableTcpPings:         false,                  // TCP pings are safe, even with mixed versions
 		AwarenessMaxMultiplier:  10,                      // Probe interval backs off to 8 seconds
 
