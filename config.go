49f08888092902e39450ccffe72223b2e41b0463
Harsha Nanayakkara <harsh.life@hotmail.com>
2025-03-28 10:25:20 +0100

    update config.go

--- config.go
+++ config.go
@@ -310,17 +310,17 @@ func DefaultLANConfig() *Config {
 		ProtocolVersion:         ProtocolVersion2Compatible,
 		TCPTimeout:              15 * time.Second,       // Timeout after 10 seconds
 		IndirectChecks:          3,                      // Use 3 nodes for the indirect ping
-		RetransmitMult:          1,                      // Retransmit a message 4 * log(N+1) nodes
-		SuspicionMult:           1,                      // Suspect a node for 4 * log(N+1) * Interval
+		RetransmitMult:          3,                      // Retransmit a message 4 * log(N+1) nodes
+		SuspicionMult:           2,                      // Suspect a node for 4 * log(N+1) * Interval
 		SuspicionMaxTimeoutMult: 8,                      // For 10k nodes this will give a max timeout of 120 seconds
-		PushPullInterval:        90 * time.Second,       // Low frequency
+		PushPullInterval:        30 * time.Second,       // Low frequency
 		ProbeTimeout:            500 * time.Millisecond, // Reasonable RTT time for LAN (How long to wait for a response when checking if a node is alive)
-		ProbeInterval:           45 * time.Second,        // Failure check every second
+		ProbeInterval:           15 * time.Second,        // Failure check every second
 		DisableTcpPings:         false,                  // TCP pings are safe, even with mixed versions
 		AwarenessMaxMultiplier:  10,                      // Probe interval backs off to 8 seconds
 
 		GossipNodes:          3,                      // Gossip to 3 nodes
-		GossipInterval:       15 * time.Second, // Gossip more rapidly
+		GossipInterval:       5 * time.Second, // Gossip more rapidly
 		GossipToTheDeadTime:  150 * time.Second,       // Same as push/pull
 		GossipVerifyIncoming: true,
 		GossipVerifyOutgoing: true,
